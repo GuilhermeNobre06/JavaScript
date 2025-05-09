578bb9815f7b84827c445facbac4b0f5213b306f
Guilherme Nobre <tayloneguis2@gmail.com>
2025-05-08 21:40:34 -0300

    exercicios de Condiçoes

---
+++ aula 1/ex008.js
@@ -0,0 +1,6 @@
+var vel = 110
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 90){
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO! ')
+}
+console.log('Dirija sempre com cinto de segurança')
