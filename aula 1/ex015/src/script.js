c6d781fc24ef2cbc1a75e1f1ea23a216a70a9a97
Guilherme Nobre <tayloneguis2@gmail.com>
2025-05-29 21:35:24 -0300

    update exmudar horario e img

--- aula 1/ex015/src/script.js
+++ aula 1/ex015/src/script.js
@@ -1,13 +1,19 @@
-var agora = new Date()
-var hora = agora.getHours()
+    const agora = new Date()
+    const hora = 15
+    const img = document.getElementById('foto')
 
-if (hora < 5) {
-    document.getElementById('hora').innerHTML = `Agora são 
-    ${hora} horas`
-} else if (hora < 12){
+    let caminho = "";
+
     document.getElementById('hora').innerHTML = `Agora são ${hora} horas`
-} else if (hora < 18){
-    document.getElementById('hora').innerHTML = `Agora são ${hora} horas`
-} else {
-    document.getElementById('hora').innerHTML = `Agora são ${hora} horas`
-}
+
+    if(hora < 5){
+        document.getElementById('b').style.backgroundColor = '#2d6c89'
+        caminho = "src/img/madruga.jpg";
+    } else if (hora < 12){
+        document.getElementById('b').style.backgroundColor = 'rgb(238, 213, 158)'
+        caminho = "src/img/nascer.jpg";
+    } else if (hora < 18){
+        document.getElementById('b').style.backgroundColor = '#FFA500'
+        caminho = "src/img/nascer.jpg";
+    }
+    img.src = caminho
