a0875b6b05cb5bd920ce52a5355bf6f7eec1ff48
Guilherme Nobre <tayloneguis2@gmail.com>
2025-06-30 21:18:02 -0300

    testando

--- aula 1/ex016/src/script.js
+++ aula 1/ex016/src/script.js
@@ -3,5 +3,7 @@ const ano = agora.getFullYear()
 const img = document.getElementById('foto')
 
 function VerificarIdade(){
+    const nasc = getElementById(nasc)
     const ano = ano - nasc 
-}
+    document.getElementById('identificador').innerHTML(`Voce tem ${ano} Anos`)
+} 
